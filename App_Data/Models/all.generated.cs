07aa08238a327e88bcaee2b1bb86272816c3f4e7
DESKTOP-44D12JS\Asus <marian.zoicas@gmail.com>
2019-09-20 14:41:26 +0200

    second commit

--- App_Data/Models/all.generated.cs
+++ App_Data/Models/all.generated.cs
@@ -9,7 +9,7 @@ using  Umbraco.ModelsBuilder;
 using  Umbraco.ModelsBuilder.Umbraco;
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "aecdb678b713c204")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.12")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "aecdb678b713c204")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.12")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedModels
 {
